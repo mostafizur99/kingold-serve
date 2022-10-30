0d99f5169deb6f0e9905b50bbf422f2864156efe
mostafizur99 <coder.mr11@gmail.com>
2022-10-30 23:48:32 +0600

    feat: cors added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 
 const app = express()
 const port = process.env.APP_PORT || 5000
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
